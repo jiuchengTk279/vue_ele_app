3e850ae2555803828deaf1b96803c7260d8aa072
jiucheng <15279232381@163.com>
2021-01-08 17:54:06 +0800

    登录组件Login 和底部导航组件Tabbar 的封装

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,34 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'index',
-    component: () => import('../views/index.vue')
+    // name: 'index',
+    component: () => import('../views/index.vue'),
+    children: [
+      {
+        path: '/',
+        redirect: '/home'
+      },
+      {
+        path: '/home',
+        name: 'home',
+        component: () => import('../views/Home.vue')
+      },
+      {
+        path: '/me',
+        name: 'me',
+        component: () => import('../views/Me.vue')
+      },
+      {
+        path: '/order',
+        name: 'order',
+        component: () => import('../views/Order.vue')
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
   }
 ]
 
@@ -15,6 +41,18 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+// 路由导航守卫
+router.beforeEach((to, from, next) => {
+  const isLoin = localStorage.ele_login ? true : false
+  // 判断去向路径是否是登录页面
+  if (to.path === '/login') {
+    next()
+  } else {
+    // 是否在登录状态下
+    isLoin ? next() : next('/login')
+  }
 })
 
 export default router
